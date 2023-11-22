423c2c7b3ce97a5909902346e39b9803496aa778
parthct45 <parthmane74112@gmail.com>
2023-11-22 11:30:25 +0530

    Added Voter_details

--- dbms_project/voters/views.py
+++ dbms_project/voters/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from .forms import RegistrationForm
 from .models import *
 from django.contrib import messages
+import random 
 
 def register(request):
     print("Received request")
@@ -28,10 +29,12 @@ def register(request):
             )
             new_voter.save()
             print("Created new user")
+
+            voter_id = new_voter.Voter_ID ; 
             # Perform any additional actions if needed
             # Redirect or render success page
             #return render(request, 'registration_success.html')
-            return redirect('/register')
+            return redirect(f'/register/details/{voter_id}')
         else:
             print("Form is invalid")
             print(form.errors)
@@ -40,6 +43,40 @@ def register(request):
         form = RegistrationForm()
 
     return render(request, 'register.html', {'form': form})
+
+
+def details(request , voter_id):
+
+    if request.method== "POST":
+        First_Name = request.POST.get('First_Name')
+        Last_Name =request.POST.get('Last_Name')
+        Contact_Number = request.POST.get('Contact_Number')
+        Address = request.POST.get('Address')
+        Date_of_Birth = request.POST.get('DOB')
+        city = request.POST.get('city')
+        print(city) 
+        temp = Constituencies.objects.get(City = city)
+        c_id = temp.Constituency_ID  
+        new_voter_details = Voter_Details.objects.create(
+                Voter_ID = voter_id , 
+                First_Name=First_Name,
+                Last_Name=Last_Name,
+                Contact_Number = Contact_Number ,
+                Address = Address ,
+                Date_of_Birth = Date_of_Birth ,
+                Constituency_ID = c_id ,
+                Voter_Card_Number = random.randint(1000000000, 9999999999)
+
+                # Verified_By=None
+            )
+        new_voter_details.save() 
+
+        return redirect('/login/') 
+        
+            
+        
+    return render(request , 'details.html') 
+
 
 
 def login_page(request):
